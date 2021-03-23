efbd8f07892c101935e39e6cef8541f9429b495e
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-03-23 14:46:13 -0300

    removed files that are not used

--- src/entities/_utils/index.ts
+++
@@ -1,5 +0,0 @@
-export function validateEmail(email: string): boolean {
-    const tester = /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
-    const result = tester.test(String(email).toLowerCase())
-    return result
-}

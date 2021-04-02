c9caf2f81806be2a56207f914d9597b905994086
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-04-02 11:50:42 -0300

    fix: configure production and local port logic

--- src/external/app/constants/index.ts
+++ src/external/app/constants/index.ts
@@ -1,3 +1,7 @@
-export const LOCAL_PORT = process.env.LOCAL_PORT
+const LOCAL_PORT = process.env.LOCAL_PORT
+const PRODUCTION_PORT = process.env.PORT
+
+export const PORT = PRODUCTION_PORT || LOCAL_PORT
+
 export const MONGO_URL = process.env.MONGO_URL
 export const DEFAULT_USER_ROLE = process.env.DEFAULT_USER_ROLE
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import 'dotenv/config'
 
 import { app } from './external/app'
-import { LOCAL_PORT } from './external/app/constants'
+import { PORT } from './external/app/constants'
 
-app.listen(LOCAL_PORT, () =>
-    console.log(`Server running on port ${LOCAL_PORT}`)
-)
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
